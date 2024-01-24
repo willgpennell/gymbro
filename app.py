fb1acc89ff119790ea6ae865081a27c145492eb3
willgpennell <wpennell48@gmail.com>
2024-01-24 19:28:25 -0330

    redoing backend

--- app.py
+++ app.py
@@ -3,34 +3,21 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-# load private api key 
+# load private api key from .env
 load_dotenv()
-OpenAI.api_key = os.environ["API_KEY"]
 
-app = Flask(__name__, template_folder='templates', static_url_path='/static')
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]
+
+app = Flask(__name__, template_folder='templates', static_url_path='static')
+
+def getResponse(prompt):
+    messages = [{"role": "system", "content": "You are a stereotypical gymbro coach who gives helpful advice"}]
+    messages.append([{"role": "user", "content":prompt}])
+
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/bot", methods=["POST"])
-def bot():
-
-    client = OpenAI()
-
-    input = request.form["message"]
-    history = []
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        max_tokens=50,
-        messages=[{"role": "system","content": "You are a helpful but stereotypical gymbro coach, you give helpful workout advice"}]
-    )
-
-    result = response['choices'][0]['message']['content']
-    prompt = ("\nUser:",{input},"\nChatbot: ",{result})
-    history.append(prompt)
-
-
-if __name__ =="__main__":
+    return
+if __name__ == "__main__":
     app.run()
