7504d3f227f4ea7a8b291d5e1282b45405644126
willgpennell <wpennell48@gmail.com>
2024-01-24 20:04:58 -0330

    very basic layout

--- app.py
+++ app.py
@@ -6,18 +6,34 @@ from dotenv import load_dotenv
 # load private api key from .env
 load_dotenv()
 
+client = OpenAI()
+
 OpenAI.api_key = os.environ["OPENAI_API_KEY"]
 
-app = Flask(__name__, template_folder='templates', static_url_path='static')
+app = Flask(__name__)
 
 def getResponse(prompt):
     messages = [{"role": "system", "content": "You are a stereotypical gymbro coach who gives helpful advice"}]
     messages.append([{"role": "user", "content":prompt}])
 
+    response = client.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        messages = messages,
+        max_tokens= 30
+    )
 
+    return response.choices[0].message.content
 
 @app.route("/")
 def home():
-    return
+    render_template('index.html')
+
+
+@app.route("/relay")
+def talk():
+    prompt = request.args.get('in')
+    responseClean = getResponse(prompt)
+    return responseClean
+
 if __name__ == "__main__":
     app.run()
